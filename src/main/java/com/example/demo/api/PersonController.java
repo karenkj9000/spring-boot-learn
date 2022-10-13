888d9edf862f1a31d70dd38b5baa58c2dd0b7279
anushree <anushreevinod10@gmail.com>
2022-10-13 17:41:56 +0530

    add delete and update api

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -27,9 +27,18 @@ class PersonController {
     List<Person> getAllPeople() {
         return personService.getAllPeople();
     }
-
     @GetMapping(path="{id}")
     public Person getPersonById(@PathVariable("id") UUID id) {
         return personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path="{id}")
+    void deletePersonById(@PathVariable("id") UUID id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path="{id}")
+    public void updatePerson(@PathVariable("id") UUID id,@RequestBody Person personToUpdate) {
+        personService.updatePerson(id, personToUpdate);
     }
 }
